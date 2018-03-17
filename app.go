0183141c3e168a929830ff548c0a1929c9507c51
figgyc <figgyc@figgyc.uk>
2018-03-17 15:46:20 +0000

    alot of changes

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,6 +18,7 @@ var view *jet.Set
 var store *mongostore.MongoStore
 var mgoSession mgo.Session
 var devices *mgo.Collection
+var connections map[string]*websocket.Conn
 
 // Device : struct for devices
 type Device struct {
@@ -24,11 +26,13 @@ type Device struct {
 	ID0        string `bson:"_id"`
 	HasMovable bool
 	HasPart1   bool
+	LFCS       []byte
 }
 
 // pool of devices for bot, and for bfers
-var botID0s *mgo.Collection
-var jobID0s *mgo.Collection
+var botFCs *mgo.Collection
+var jobDevices *mgo.Collection
+var workingDevices *mgo.Collection
 
 func renderTemplate(template string, vars jet.VarMap, request *http.Request, writer http.ResponseWriter, context interface{}) {
 	t, err := view.GetTemplate(template)
@@ -54,8 +58,9 @@ func main() {
 	store = mongostore.NewMongoStore(mgoSession.DB("sessions").C("sessions"), 86400, true, []byte(os.Getenv("SESSION_SECRET")))
 
 	devices = mgoSession.DB("main").C("devices")
-	botID0s = mgoSession.DB("main").C("botID0s")
-	jobID0s = mgoSession.DB("main").C("jobID0s")
+	botFCs = mgoSession.DB("main").C("botFCs")
+	jobDevices = mgoSession.DB("main").C("jobDevices")
+	workingDevices = mgoSession.DB("main").C("workingDevices")
 
 	// init templates
 	view = jet.NewHTMLSet("./views")
@@ -70,6 +75,7 @@ func main() {
 		renderTemplate("home", make(jet.VarMap), r, w, nil)
 	})
 
+	// client:
 	upgrader := websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
@@ -88,17 +94,243 @@ func main() {
 				log.Println(err)
 				return
 			}
-			if err := conn.WriteMessage(messageType, p); err != nil {
+			if messageType == websocket.TextMessage {
+				var object map[string]interface{}
+				err := json.Unmarshal(p, object)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				if object["id0"] == nil {
+					return
+				}
+				isRegistered := false
+				for _, v := range connections {
+					if v == conn {
+						isRegistered = true
+					}
+				}
+				if isRegistered == false {
+					connections[object["id0"].(string)] = conn
+				}
+
+				if object["request"] == "bruteforce" {
+					// add to BF pool
+					query := jobDevices.Find(Device{ID0: object["id0"].(string)})
+					count, err := query.Count()
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					if count > 1 {
+						var device Device
+						err = query.One(device)
+						if err != nil {
+							log.Println(err)
+							return
+						}
+						if device.HasPart1 == true {
+							err = jobDevices.Insert(device)
+							if err != nil {
+								log.Println(err)
+								return
+							}
+						}
+					} else {
+						return
+					}
+				} else if object["friendCode"] != nil {
+					// add to bot pool
+					// TODO: verify fc
+					device := Device{FriendCode: object["friendCode"].(string), ID0: object["id0"].(string)}
+					err = devices.Insert(device)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					err = botFCs.Insert(object["friendCode"].(string))
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					msg := "{\"status\": \"friendCodeAdded\"}"
+					if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+						log.Println(err)
+						return
+					}
+				} else {
+					// checc
+					query := jobDevices.Find(Device{ID0: object["id0"].(string)})
+					count, err := query.Count()
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					if count > 1 {
+						var device Device
+						err = query.One(device)
+						if err != nil {
+							log.Println(err)
+							return
+						}
+						if device.HasMovable == true {
+							msg := "{\"status\": \"movablePart1\"}"
+							if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+								log.Println(err)
+								return
+							}
+						} else if device.HasPart1 == true {
+							msg := "{\"status\": \"done\"}"
+							if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+								log.Println(err)
+								return
+							}
+						} else {
+							if device.HasPart1 == true {
+								msg := "{\"status\": \"friendCodeAdded\"}"
+								if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+									log.Println(err)
+									return
+								}
+							}
+						}
+					} else {
+						return
+					}
+				}
+			} else if messageType == websocket.CloseMessage {
+				for k, v := range connections {
+					if v == conn {
+						delete(connections, k)
+					}
+				}
+			}
+
+		}
+	})
+
+	// part1 auto script:
+	// /getfcs
+	router.HandleFunc("/getfcs", func(w http.ResponseWriter, r *http.Request) {
+		query := botFCs.Find(nil)
+		count, err := query.Count()
+		if err != nil || count < 1 {
+			w.Write([]byte("nothing"))
+			return
+		}
+		var fcs []string
+		err = query.All(fcs)
+		if err != nil || len(fcs) < 1 {
+			w.Write([]byte("nothing"))
+			return
+		}
+
+		for _, fc := range fcs {
+			w.Write([]byte(fc))
+			w.Write([]byte("\n"))
+		}
+		return
+	})
+	// /added/fc
+	router.HandleFunc("/added/{fc}", func(w http.ResponseWriter, r *http.Request) {
+		fc := mux.Vars(r)["fc"]
+
+		query := jobDevices.Find(Device{FriendCode: fc})
+		count, err := query.Count()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if count > 1 {
+			var device Device
+			err = query.One(device)
+			if err != nil {
 				log.Println(err)
 				return
+			}
+			for id0, conn := range connections {
+				if id0 == device.ID0 {
+					msg := "{\"status\": \"friendCodeAdded\"}"
+					if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+						log.Println(err)
+						return
+					}
+				}
 			}
 		}
 	})
 
+	// /lfcs/fc
+	// get param lfcs is lfcs as hex escaped eg %10%22%65%00 or whatevs
+	router.HandleFunc("/lfcs/{fc}", func(w http.ResponseWriter, r *http.Request) {
+		fc := mux.Vars(r)["fc"]
+		lfcs, ok := r.URL.Query()["lfcs"]
+		if ok == false {
+			return
+		}
+
+		query := jobDevices.Find(Device{FriendCode: fc})
+		count, err := query.Count()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if count > 1 {
+			var device Device
+			err = query.One(device)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			device.LFCS = []byte(lfcs[0])
+			err = devices.Insert(device)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			err = jobDevices.Remove(Device{FriendCode: fc})
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			for id0, conn := range connections {
+				if id0 == device.ID0 {
+					msg := "{\"status\": \"movablePart1\"}"
+					if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+						log.Println(err)
+						return
+					}
+				}
+			}
+		}
+	})
+
+	// msed auto script:
 	// /cancel/id0
+	router.HandleFunc("/cancel/{id0}", func(w http.ResponseWriter, r *http.Request) {
+		id0 := mux.Vars(r)["id0"]
+		fmt.Println(id0)
+	})
 	// /getwork
+	router.HandleFunc("/getwork", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+	// /claim/id0
+	router.HandleFunc("/claim/{id0}", func(w http.ResponseWriter, r *http.Request) {
+		id0 := mux.Vars(r)["id0"]
+		fmt.Println(id0)
+	})
 	// /part1/id0
-	// /upload/id0
+	// this is also used by client if they want self BF
+	router.HandleFunc("/part1/{id0}", func(w http.ResponseWriter, r *http.Request) {
+		id0 := mux.Vars(r)["id0"]
+		fmt.Println(id0)
+	})
+	// POST /upload/id0 w/ file movable
+	router.HandleFunc("/upload/{id0}", func(w http.ResponseWriter, r *http.Request) {
+		id0 := mux.Vars(r)["id0"]
+		fmt.Println(id0)
+	})
 
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		renderTemplate("404error", make(jet.VarMap), r, w, nil)
--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,49 @@
 // hi! this script is what automatically recieves when things are finished.
 
-/*
-    Step 1: gather user data
-    friendCode input box
-    id0 input box
-    beginButton button
-*/
+
 
 let socket = new WebSocket("wss://seedhelper.figgyc.uk/socket")
 socket.addEventListener("message", (e) => {
-    if (JSON.parse(e.data) == { "status": "friendCodeAdded" }) {
+    if (JSON.parse(e.data).status == "friendCodeAdded") {
         /* 
             Step 2: tell the user to add the bot
         */
        document.getElementById("collapseOne").classList.remove("show")
        document.getElementById("collapseTwo").classList.add("show")
     }
-    if (JSON.parse(e.data) == { "status": "movablePart1" }) {
+    if (JSON.parse(e.data).status == "movablePart1") {
         /*
             Step 3: ask user if they or worker wants to BF
             continue button
+            downloadPart1 a
         */
        document.getElementById("collapseTwo").classList.remove("show")
        document.getElementById("collapseThree").classList.add("show")
+       document.getElementById("downloadPart1").href = "/part1/" + localStorage.getItem("id0")
+    }
+    if (JSON.parse(e.data).status == "done") {
+        /* 
+            Step 5: done! 
+            downloadMovable a
+        */
+        document.getElementById("collapseFour").classList.remove("show")
+        document.getElementById("collapseFive").classList.add("show")
+        document.getElementById("downloadMovable").href = "/movable/" + localStorage.getItem("id0")
     }
 })
 if (localStorage.getItem("id0") != null) {
     // send intro packet, if we get anything back then stuff will happen
     socket.send(JSON.stringify({
-        id0: localStorage.getItem("id0"),
+        id0: localStorage.getItem("id0")
     }))
 }
 
+/*
+    Step 1: gather user data
+    friendCode input box
+    id0 input box
+    beginButton button
+*/
 document.getElementById("beginButton").addEventListener("click", () => {
     //document.getElementById("").setAttribute()
     socket.send(JSON.stringify({
@@ -39,4 +51,18 @@ document.getElementById("beginButton").addEventListener("click", () => {
         id0: document.getElementById("id0").value,
     }))
     localStorage.setItem("id0", document.getElementById("id0").value)
+})
+
+/*
+    Step 4: wait for BF
+    continue button
+*/
+document.getElementById("continue").addEventListener("click", () => {
+    //document.getElementById("").setAttribute()
+    socket.send(JSON.stringify({
+        request: "bruteforce",
+        id0: localStorage.getItem("id0"),
+    }))
+    document.getElementById("collapseThree").classList.remove("show")
+    document.getElementById("collapseFour").classList.add("show")
 })
