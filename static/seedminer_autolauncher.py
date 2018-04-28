aa052f8e9395ac3ae94540664a805931e12f1960
figgyc <figgyc@figgyc.uk>
2018-04-28 20:41:35 +0100

    attempt to handle errors

--- static/seedminer_autolauncher.py
+++ static/seedminer_autolauncher.py
@@ -16,7 +16,7 @@ import io
 s = requests.Session()
 baseurl = "https://seedhelper.figgyc.uk"
 currentid = ""
-currentVersion = "1.8"
+currentVersion = "1.9"
 
 # https://stackoverflow.com/a/16696317 thx
 def download_file(url, local_filename):
@@ -70,65 +70,69 @@ signal.signal(signal.SIGINT, signal_handler)
 
 
 while True:
-    print("Finding work...")
     try:
-        r = s.get(baseurl + "/getwork")
-    except:
-        print("Error. Waiting 30 seconds...")
-        time.sleep(30)
-        continue        
-    if r.text == "nothing":
-        print("No work. Waiting 30 seconds...")
-        time.sleep(30)
-    else:
-        currentid = r.text
-        r2 = s.get(baseurl + "/claim/" + currentid)
-        if r2.text == "error":
-            print("Device already claimed, trying again...")
+        print("Finding work...")
+        try:
+            r = s.get(baseurl + "/getwork")
+        except:
+            print("Error. Waiting 30 seconds...")
+            time.sleep(30)
+            continue        
+        if r.text == "nothing":
+            print("No work. Waiting 30 seconds...")
+            time.sleep(30)
         else:
-            print("Downloading part1 for device " + currentid)
-            download_file(baseurl + '/part1/' + currentid, 'movable_part1.sed')
-            print("Bruteforcing " + str(datetime.datetime.now()))
-            process = subprocess.Popen([sys.executable, "seedminer_launcher3.py", "gpu"], creationflags=0x00000200)#, stdout=subprocess.PIPE, universal_newlines=True)
-            timer = 0
-            #stdout = open(process.stdout)
-            while process.poll() == None:
-                # we need to poll for kill more often then we check server because we would waste up to 30 secs after finish
-                timer = timer + 1
-                time.sleep(1)
-                #line = process.stdout.read()
-                #print(line)
-                #if "offset:250" in line:
-                #    print("Job taking too long, killing...")
-                #    s.get(baseurl + "/cancel/" + currentid)
-                #    subprocess.call(['taskkill', '/F', '/T', '/IM', 'bfcl.exe'])
-                #    break
-                if timer % 30 == 0:
-                    r3 = s.get(baseurl + "/check/" + currentid)
-                    if r3.text != "ok":
-                        print("Job cancelled or expired, killing...")
-                        # process.kill() broke
-                        subprocess.call(['taskkill', '/F', '/T', '/IM', 'bfcl.exe'])
+            currentid = r.text
+            r2 = s.get(baseurl + "/claim/" + currentid)
+            if r2.text == "error":
+                print("Device already claimed, trying again...")
+            else:
+                print("Downloading part1 for device " + currentid)
+                download_file(baseurl + '/part1/' + currentid, 'movable_part1.sed')
+                print("Bruteforcing " + str(datetime.datetime.now()))
+                process = subprocess.Popen([sys.executable, "seedminer_launcher3.py", "gpu"], creationflags=0x00000200)#, stdout=subprocess.PIPE, universal_newlines=True)
+                timer = 0
+                #stdout = open(process.stdout)
+                while process.poll() == None:
+                    # we need to poll for kill more often then we check server because we would waste up to 30 secs after finish
+                    timer = timer + 1
+                    time.sleep(1)
+                    #line = process.stdout.read()
+                    #print(line)
+                    #if "offset:250" in line:
+                    #    print("Job taking too long, killing...")
+                    #    s.get(baseurl + "/cancel/" + currentid)
+                    #    subprocess.call(['taskkill', '/F', '/T', '/IM', 'bfcl.exe'])
+                    #    break
+                    if timer % 30 == 0:
+                        r3 = s.get(baseurl + "/check/" + currentid)
+                        if r3.text != "ok":
+                            print("Job cancelled or expired, killing...")
+                            # process.kill() broke
+                            subprocess.call(['taskkill', '/F', '/T', '/IM', 'bfcl.exe'])
+                            currentid = ""
+                            print("press ctrl-c to quit")
+                            time.sleep(5)
+                            continue
+                #os.system('"' + sys.executable + '" seedminer_launcher3.py gpu')
+                if os.path.isfile("movable.sed"):
+                    print("Uploading")
+                    # seedhelper2 has no msed database but we upload these anyway so zoogie can have them
+                    list_of_files = glob.glob('msed_data_*.bin') # * means all if need specific format then *.csv
+                    latest_file = max(list_of_files, key=os.path.getctime)
+                    ur = s.post(baseurl + '/upload/' + currentid, files={'movable': open('movable.sed', 'rb'), 'msed': open(latest_file, 'rb')})
+                    print(ur)
+                    if ur.text == "success":
+                        print("Upload succeeded!")
+                        os.remove("movable.sed")
+                        os.remove(latest_file)
                         currentid = ""
                         print("press ctrl-c to quit")
                         time.sleep(5)
-                        continue
-            #os.system('"' + sys.executable + '" seedminer_launcher3.py gpu')
-            if os.path.isfile("movable.sed"):
-                print("Uploading")
-                # seedhelper2 has no msed database but we upload these anyway so zoogie can have them
-                list_of_files = glob.glob('msed_data_*.bin') # * means all if need specific format then *.csv
-                latest_file = max(list_of_files, key=os.path.getctime)
-                ur = s.post(baseurl + '/upload/' + currentid, files={'movable': open('movable.sed', 'rb'), 'msed': open(latest_file, 'rb')})
-                print(ur)
-                if ur.text == "success":
-                    print("Upload succeeded!")
-                    os.remove("movable.sed")
-                    os.remove(latest_file)
-                    currentid = ""
-                    print("press ctrl-c to quit")
-                    time.sleep(5)
-                else:
-                    print("Upload failed!")
-                    sys.exit(1)
+                    else:
+                        print("Upload failed!")
+                        sys.exit(1)
+    except:
+        print("Error")
+        time.sleep(10)
 
